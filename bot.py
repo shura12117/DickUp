b675dd828ba905ad2691813affd9f3d89e412084
Bazuka <suradaniil74@gmail.com>
2026-09-16 13:43:50 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -140,7 +140,7 @@ async def process_wank(interaction: discord.Interaction):
         
         await interaction.followup.send(
             f"**{user.name}**, подро🍆ил 😈\n"
-            f"Дро🍆ек всего (глобально) - **{count}**"
+            f"Дро🍆ек всего - **{count}**"
         )
         
     except Exception as e:
